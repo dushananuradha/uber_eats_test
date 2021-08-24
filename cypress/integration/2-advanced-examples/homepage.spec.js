10705b6e7a6756c555734827f2fbff6e3bce4c33
danb123 <ohda456@gmail.com>
2021-08-24 22:36:54 +0530

    homepage_spec_v2

--- cypress/integration/2-advanced-examples/homepage.spec.js
+++ cypress/integration/2-advanced-examples/homepage.spec.js
@@ -5,6 +5,12 @@ import HomePage from "../uber_eats_tests/HomePage"
 describe('HomePage_Test_Suite', ()=> {
 
     it("navigation_homepage", ()=> {
+        //TC_01
         HomePage.loadHomePage();
+    })
+
+    it("change_delivery_address", ()=> {
+        //TC_02
+        HomePage.searchDeliveryAddress();
     })
 })
--- cypress/integration/uber_eats_tests/HomePage.js
+++ cypress/integration/uber_eats_tests/HomePage.js
@@ -5,6 +5,18 @@ class HomePage {
         cy.visit(Cypress.env('url'))
         //cy.url().should('include', 'ubereats')
     }
+
+    static searchDeliveryAddress(){
+        cy.xpath('//input[@id="location-typeahead-home-input"]').click({force:true}).type('bamba')
+        cy.xpath('//*[@id="location-typeahead-home-item-0"]/div[3]/div[2]').then(($text2) =>{
+            if($text2.text() === "Colombo, Sri Lanka"){
+                cy.wrap($text2).click({force: true})
+            }
+        })
+    }
 }
+
+
+
 
 export default HomePage;
